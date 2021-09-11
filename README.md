484bcba2fa2729ef80dada88f288d07cb90856e7
Jihogrammer <jiho@kakao.com>
2021-09-11 18:38:21 +0900

    feat: why reactive

--- README.md
+++ README.md
@@ -1,7 +1,36 @@
 # Toby의 Reactive Functional Programming
 
-> 개념
-> - Duality
-> - Observer Pattern
-> - Reactive Streams : Java9 API 표준
+>   개념
+>   - Duality
+>   - Observer Pattern
+>   - Reactive Streams : Java9 API 표준
 
+## Duality
+
+>   에릭 마이어 (Duality, 쌍대성)
+>   - Iterable <----> Observable
+>   - Pulling 방식 : next()로 계속 다음 
+
+### Iterable vs Observable
+
+| Iterable | Observable              |
+| -------- | ----------------------- |
+| Pull     | Push                    |
+| `next()` | `notifyObservers(args)` |
+
+### Observable의 장단점
+
+#### 장점
+
+-   별도의 Thread로 동작할 때 여러 이점이 있다.
+-   다수의 Subscriber에게 BroadCasting이 가능하다.
+
+#### 단점
+
+-   Complete이라는 개념을 적용하기 어렵다.
+    >   언제까지 계속 돌아가는데? 
+-   Error/Exception 처리가 어렵다.
+    >   단순하게 생각하면 try-catch 하면 되지 싶다.
+    >   하지만, 어떻게 복구하고 처리할 것인지 생각해야 한다.
+
+이러한 단점을 극복해서 나온 것이 Reactive Observer라고 한다.
---
+++ src/main/java/toby/live/Reactive/PubSub.java
@@ -0,0 +1,101 @@
+package toby.live.Reactive;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Flow.Publisher;
+import java.util.concurrent.Flow.Subscriber;
+import java.util.concurrent.Flow.Subscription;
+import java.util.concurrent.TimeUnit;
+
+public class PubSub {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // Publisher : Observable
+        // Subscriber : Observer
+
+        Iterable<Integer> iterable = Arrays.asList(1, 2, 3, 4, 5);
+        ExecutorService es = Executors.newCachedThreadPool();
+
+        Publisher publisher = new Publisher() {
+
+            Iterator<Integer> iterator = iterable.iterator();
+
+            @Override
+            public void subscribe(Subscriber subscriber) {
+                subscriber.onSubscribe(new Subscription() {
+                    @Override
+                    public void request(long n) {
+                        es.execute(() -> {
+                            int i = 0;
+                            try {
+                                while (i++ < n) {
+                                    if (iterator.hasNext()) {
+                                        subscriber.onNext(iterator.next());
+                                    } else {
+                                        subscriber.onComplete();
+                                        break;
+                                    }
+                                }
+                            } catch (RuntimeException e) {
+                                subscriber.onError(e);
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void cancel() {
+
+                    }
+                });
+            }
+        };
+
+        Subscriber subscriber = new Subscriber() {
+
+            Subscription subscription;
+//            int bufferSize = 2;
+
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println(Thread.currentThread().getName() + " [onSubscribe] " + subscription);
+//                subscription.request(Long.MAX_VALUE);
+                this.subscription = subscription;
+//                this.subscription.request(this.bufferSize);
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onNext(Object item) {
+                // like update() of Observer Pattern
+                System.out.println(Thread.currentThread().getName() + " [onNext] " + item);
+//                if (--this.bufferSize <= 0) {
+//                    this.bufferSize = 2;
+//                    this.subscription.request(this.bufferSize);
+//                }
+                this.subscription.request(1);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                // 별도의 try-catch 구문을 사용하지 않고
+                // 이 메소드로 Exception 처리
+                System.out.println(Thread.currentThread().getName() + " [onError] " + throwable.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println(Thread.currentThread().getName() + " [onComplete] Completed!");
+            }
+        };
+
+        publisher.subscribe(subscriber);
+
+        es.awaitTermination(10, TimeUnit.HOURS);
+        es.shutdown();
+
+    }
+
+}
